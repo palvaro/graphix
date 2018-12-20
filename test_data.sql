993330c7a4df0d9f962abc24a1508258193e9741
palvaro <pealvaro@ebay.com>
2018-12-20 11:12:34 -0800

    flush

--- test_data.sql
+++ test_data.sql
@@ -2,8 +2,10 @@ insert into graphs values (1, 1, 0);
 insert into nodes values (1, 1, "A");
 insert into nodes values (1, 2, "B");
 insert into nodes values (1, 3, "C");
+insert into nodes values (1, -3, "D");
 insert into edges values (1, 2, NULL);
 insert into edges values (1, 3, NULL);
+insert into edges values (3, -3, NULL);
 
 insert into graphs values (2, 1, 0);
 insert into nodes values (2, 4, "A");
@@ -22,4 +24,15 @@ insert into nodes values (3, 10, "X");
 
 insert into edges values (8, 9, NULL);
 insert into edges values (8, 10, NULL);
+
+
+insert into graphs values (4,1, 0);
+insert into nodes values (4, 11, "A");
+insert into nodes values (4, 12, "X");
+insert into nodes values (4, 13, "B");
+insert into nodes values (4, 14, "D");
+
+insert into edges values (11, 12, NULL);
+insert into edges values (11, 13, NULL);
+insert into edges values (12, 14, NULL);
 
